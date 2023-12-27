0ce82dc25c68d435b64b506fbd0c8a99d5a5dfda
yChaaby <113364055+yChaaby@users.noreply.github.com>
2023-12-27 21:25:32 +0100

    f

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from Univert import Univert, Etoile
 
 
-solar_system = Univert(350)
+solar_system = Univert(550)
 body = Etoile(solar_system, 10,100,couleur="Pink",position=(2,1,-1) ,vitesse=(2,2,2))
 body = Etoile(solar_system, 50,50,couleur="Red" ,vitesse=(1,0, 0))
 body = Etoile(solar_system, 50,50,couleur="orange",position=(4,1,-1) ,vitesse=(1,0, 0))
@@ -13,5 +13,5 @@ while True:
     solar_system.interaction_calculateur()
     solar_system.update_all()
     solar_system.draw_all()
-    ##print(body.vitesse)
+    print(body.vitesse)
     
